5df793767c3d99e94abac3da6dfc325847a15988
Tobias Roenningen <tobias.ronningen@gmail.com>
2015-03-10 23:32:23 +0100

    changed structure a bit for admin.py, just a tad

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from polls.models import Question
 from polls.models import Choice, Question
-# ...
-admin.site.register(Choice)
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -12,3 +9,4 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
